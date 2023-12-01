ecce6e0cb9db152eaf8ac59631b0eb8ad326f9d4
Becky <bjentwistle@gmail.com>
2023-12-01 18:29:07 +0000

    first attempt made an error

--- day1.py
+++ day1.py
@@ -1,4 +1,27 @@
 file = open("advent_day1.txt", "r")
 content=file.readlines()
-print(content)
 file.close()
+
+def remove_characters(array):
+    data = []
+    for word in array:
+        newWord = word.rstrip('\n')
+        data.append(newWord)
+    return data
+
+data = remove_characters(content)
+
+def find_digits(array):
+    digits = []
+    for word in array:
+        for char in word:
+           if char.isdigit():
+                digits.append(int(char))
+    return digits
+
+digits = find_digits(data)
+
+total = sum(digits)
+print(total)
+
+        
